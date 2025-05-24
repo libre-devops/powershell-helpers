4546a46ecd3a6e46a579271edd2894f152d53c37
Craig Thacker <craigthackerx@gmail.com>
2025-05-24 16:14:05 +0100

    update nuget api

--- Publish-ToAzureArtifacts.ps1
+++ Publish-ToAzureArtifacts.ps1
@@ -11,7 +11,7 @@ $repoName      = "AzureArtifacts"
 $aadToken      = $env:AZDO_ARTIFACTS_PAT  # Use a Personal Access Token with Packaging (Read & Write)
 
 # Build URI (use project-scoped if needed)
-$azureUri = "https://pkgs.dev.azure.com/$organization/_packaging/$feedName/nuget/v2"  # or with project: /$project/_packaging/...
+$azureUri = "https://pkgs.dev.azure.com/$organization/_packaging/$feedName/nuget/v3"  # or with project: /$project/_packaging/...
 
 # Register repository
 if (-not (Get-PSResourceRepository -Name $repoName -ErrorAction SilentlyContinue)) {
